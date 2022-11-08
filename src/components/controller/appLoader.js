6dbfd7e1fab6a0fbdf84343f52c86d7caf30bcc1
birbwatcher <d@rkmind.cc>
2022-11-08 17:09:27 +0100

    feat: api key added

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '24ac565ecd4340ea945f1300c2b9e5e9', // получите свой ключ https://newsapi.org/
         });
     }
 }
