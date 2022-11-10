5e35a1430d8d77f315c7a0c6ac33623ea837c0d3
birbwatcher <d@rkmind.cc>
2022-11-10 16:36:49 +0100

    feat: sources.js converted to ts

---
+++ src/components/view/sources/sources.ts
@@ -0,0 +1,30 @@
+import './sources.css';
+interface NewsSource  {
+    category: string;
+    country: string;
+    description: string;
+    id: string;
+    language: string;
+    name: string;
+    url: string;}
+
+
+class Sources {
+    draw(data: NewsSource[]): void {
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
+
+        data.forEach((item) => {
+            const sourceClone: HTMLElement = sourceItemTemp?.content.cloneNode(true) as HTMLElement;
+            const sourceItem: HTMLElement | null = sourceClone?.querySelector('.source__item-name');
+            sourceItem!.textContent = item.name;
+            sourceClone.querySelector('.source__item')?.setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        document.querySelector('.sources')?.append(fragment);
+    }
+}
+
+export default Sources;
