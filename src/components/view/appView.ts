0fae6a0ebb8395d2ad5f94e7b42b26339e3ad913
birbwatcher <d@rkmind.cc>
2022-11-11 16:20:24 +0100

    feat: appView.js converted to ts

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,18 +1,28 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { NewsSource } from './sources/sources';
+
+interface DrawData {
+    sources: NewsSource[],
+    articles: News[],
+}
 
 export class AppView {
+    news: News;
+    sources: Sources;
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: DrawData) {
+        console.log(data, 'data1')
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: DrawData) {
+        console.log(data, 'data2')
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
